195f327ef1e4dfe479e677fe86a7704ed89dc4f8
emregeziciler <emregeziciler8@gmail.com>
2025-05-26 23:34:54 +0300

    Cursor için render hamlesi

--- doksan_plus/doksan_plus/settings.py
+++ doksan_plus/doksan_plus/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h953u789n1++#y0s(2oz0)y+4pn=pma)@0i*(n5=ca!@in5)m$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-h953u789n1++#y0s(2oz0)y+4pn=pma)@0i*(n5=ca!@in5)m$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Update this with your actual domain in production
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add whitenoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -129,7 +132,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom user model
 AUTH_USER_MODEL = 'skorlar.User'
 
-API_FOOTBALL_KEY = os.getenv('1fe6442495bf6a88ab6a95cfcb3cb436')
+# Security settings
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+# API Football Key
+API_FOOTBALL_KEY = os.getenv('API_FOOTBALL_KEY', '1fe6442495bf6a88ab6a95cfcb3cb436')
 
 # Önbellek için basit ayar
 CACHES = {
--- doksan_plus/doksan_plus/wsgi.py
+++ doksan_plus/doksan_plus/wsgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from skorlar import app  # Uygulama adını doğru yaz!
+
+if _name_ == "_main_":
+    app.run()
+
+# Gunicorn bunu arıyor
+application = app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doksan_plus.settings')
 
 application = get_wsgi_application()
+
+
