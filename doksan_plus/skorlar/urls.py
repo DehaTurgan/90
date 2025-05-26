ab188ee2ac045f4204dd432b2fbd95b094556b86
emregeziciler <emregeziciler8@gmail.com>
2025-05-26 20:51:02 +0300

    Favori yıldızı değişti, son rötuşlar yapıldı.

--- doksan_plus/skorlar/urls.py
+++ doksan_plus/skorlar/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.fixtures_view, name='home'),
     path('fixtures/', views.fixtures_view, name='fixtures'),
     path('match/<int:match_id>/', views.match_detail_view, name='match_detail'),
+    path('update_language/', views.update_language, name='update_language'),
 ]
